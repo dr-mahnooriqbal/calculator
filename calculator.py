65eda769fc38aaf156a5c0fa4512cec21c02ad96
Mahnoor Iqbal <mahnoor.iqbal456@gmail.com>
2024-01-20 14:36:23 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 num1=int(input("enter a number"))
-print("select any operand from the list \n + \n - \n / \n * \n % ")
-operand=input("write the selected operand")
+print(" + \n - \n / \n * \n % ")
+operand=input("select any operand from the list above")
 num2=int(input("write a number"))
 if operand=="+":
          print (num1+num2)
